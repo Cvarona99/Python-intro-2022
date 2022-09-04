c3014430bd046b3d133e5a61cc12eef6b2271bb0
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-04 21:51:52 +0000

    Sun, Sep 4, 2022, 9:51 PM +00:00

--- week1/marioPractice.c
+++ week1/marioPractice.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include<cs50.h>
+
 
 int main(void)
 {
-    printf("????\n");
+    int n;
+    do
+    {
+        n= get_int("Width: ");
+    }
+    while (n < 1);
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
 }
