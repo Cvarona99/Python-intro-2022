58375d1d9b9c961c6c5e08cee240fd149ba9af33
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-04 19:37:53 +0000

    Sun, Sep 4, 2022, 7:37 PM +00:00

--- calculator.c
+++ calculator.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-    //prompt for x and Y 
+    //prompt for x and Y
     int x = get_int("x: ");
     int y = get_int("y: ");
     int z = x+y;
-    printf("%i\n", z);
+    printf(" = %i\n", z);
 
 }
