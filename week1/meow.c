3b39cff2834288172fb27f97a75087d3e12b60d4
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-04 20:43:27 +0000

    Sun, Sep 4, 2022, 8:43 PM +00:00

--- week1/meow.c
+++ week1/meow.c
@@ -1,17 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
-
-void meow(void)
-{
-    printf("meow\n");
-}
+void meow(void);
 
 int main(void)
 {
     for(int i = 0; i < 3; i++)
     {
-        meow(); 
+        meow();
     }
 }
 
+void meow(void)
+{
+    printf("meow\n");
+}
 
