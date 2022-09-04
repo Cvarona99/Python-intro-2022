6323a9e6e7875faccf035baac8fc4e2de8d3f874
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-04 23:23:22 +0000

    Sun, Sep 4, 2022, 11:23 PM +00:00

--- week1/cash/cash.c
+++ week1/cash/cash.c
@@ -37,8 +37,14 @@ int main(void)
 
 int get_cents(void)
 {
-    // TODO
-    return 0;
+    int cents;
+    do
+    {
+        cents = get_int("Cents Owed: ")
+    }
+    while(cents<0);
+
+    return cents;
 }
 
 int calculate_quarters(int cents)
