1a9b1d6729fdf4c0bea768a33973706fe7a5a398
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-05 20:14:46 +0000

    Mon, Sep 5, 2022, 8:14 PM +00:00

--- argv.c
+++ argv.c
@@ -4,5 +4,12 @@
 int main( int argc, string argv[])
 // Write a program that types the user's name based on input
 {
-    printf("hello, %s\n", argv[1]);
+    if(argc == 2)
+    {
+      printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello world\n");
+    }
 }
