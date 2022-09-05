301f0f217cfb0c3899743d367c9998361aa601fc
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-05 01:13:35 +0000

    Mon, Sep 5, 2022, 1:13 AM +00:00

--- week2/buggy.c
+++ week2/buggy.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
-{
-    for(int i= 0; i <=3; i++)
-    {
+{}
 
-        printf("#\n");
-    }
-}
+int = get_negative_int();
+printf("%i\n", i);
