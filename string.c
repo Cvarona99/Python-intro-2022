8bdf6d4be3269fbe02bec11647e7b00243999ebc
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-05 19:35:46 +0000

    Mon, Sep 5, 2022, 7:35 PM +00:00

--- string.c
+++ string.c
@@ -7,7 +7,8 @@ int main(void)
 
     string s = get_string("Input: ");
     printf("Output: ");
-    for (int i = 0; i < strlen(s); i++)
+
+    for (int i = 0, n = strlen(s); i < length; i++)
     {
         printf("%c", s[i]);
     }
