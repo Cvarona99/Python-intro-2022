23b6c77cb1758dcfa1ade1cbdfe63a854d9c802b
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-05 02:45:52 +0000

    Mon, Sep 5, 2022, 2:45 AM +00:00

--- week2/length.c
+++ week2/length.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int string_length(string s);
 int main(void)
 {
     string name= get_string("Name: ");
+    int length= string_length(name);
+    printf("%i\n", length);
 
-    int i = 0;
-    while (name[i] != '\0' )
+}
+
+int string_length(string s)
+{
+     int i = 0;
+    while (s[i] != '\0' )
     {
         i++;
     }
-    printf("%i\n", i);
+    return i;
 
 }
