5a1a2ed218638a4450334950cbb1ff30f2a60363
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-06 17:47:19 +0000

    Tue, Sep 6, 2022, 5:47 PM +00:00

---
+++ tutorials/createFunction.c
@@ -0,0 +1,23 @@
+// This code calculates the factorial of a number
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+
+    int n = 5;
+    int factorial = 1;
+
+    for (int i = n; i > 1; i--)
+    {
+
+        factorial *= i
+
+    }
+
+
+
+    return 0;
+
+}
