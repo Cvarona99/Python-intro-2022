51b77864803fb40d2ee9823976092e3cf1de9332
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-06 04:25:14 +0000

    Tue, Sep 6, 2022, 4:25 AM +00:00

--- tutorials/arrayLoop.c
+++ tutorials/arrayLoop.c
@@ -5,4 +5,13 @@ int main()
 {
     int size = 5;
     int myGrades[] = {size};
-    myGrades[0]
+    myGrades[0] = 10;
+    myGrades[1] = 15;
+    myGrades[2] = 20;
+    myGrades[3] = 25;
+    myGrades[4] = 30;
+
+for{
+    
+}
+    printf("%i\n", myGrades[]);
