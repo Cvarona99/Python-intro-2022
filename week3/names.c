a74e9946a89b4fe592bcbebb17a64ff6c2f1c6b9
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-07 19:55:35 +0000

    Wed, Sep 7, 2022, 7:55 PM +00:00

--- week3/names.c
+++ week3/names.c
@@ -4,16 +4,16 @@
 
 int main()
 {
-    string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"}
+    string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
 
     for (int i = 0; i < 7; i++)
     {
-        if (names[i] == "Ron")
+        if (strcmp(names[i], "Ron") == 0)
         {
             printf("Found!\n");
             return 0;
         }
     }
     printf("Not found\n");
-    return 1; 
+    return 1;
 }
