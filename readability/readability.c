7c1e6e00eaac00e17dd6ff7c064aa552912bc1e0
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-07 01:20:12 +0000

    Wed, Sep 7, 2022, 1:20 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 int main(void)
 {
@@ -28,6 +29,6 @@ int main(void)
     }
     float L = letters / words *100;
     float S = sentences / words *100;
-    
-    int index = 0.0588 * L - 0.296 * S - 15.8;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
 }
