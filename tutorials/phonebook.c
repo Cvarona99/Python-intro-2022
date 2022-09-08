31c022e4cfa119f81727cdcd2d3ea9ace2f4323d
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-08 22:10:47 +0000

    Thu, Sep 8, 2022, 10:10 PM +00:00

--- tutorials/phonebook.c
+++ tutorials/phonebook.c
@@ -4,14 +4,16 @@
 
 int main()
 {
-    string name = get_string ("What is your name?: \n")
+    string name = get_string ("What is your name?: \n");
+
+    int age = get_int ("What is your age?: \n");
+    string number = get_string ("What is your number?: \n");
+
+
 
     printf("%s\n", name);
-
-    int age = get_int ("What is your age?: \n")
-
     printf("%i\n", age);
-
+    printf("%s\n", number);
 
 
 
