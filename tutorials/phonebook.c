1f477aebc395d2347b29ef39acbedc3911596a42
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-08 22:08:19 +0000

    Thu, Sep 8, 2022, 10:08 PM +00:00

---
+++ tutorials/phonebook.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main()
+{
+    string name = get_string ("What is your name?: \n")
+
+    printf("%s\n", name);
+
+    int age = get_int ("What is your age?: \n")
+
+    printf("%i\n", age);
+
+
+
+
+
+}
