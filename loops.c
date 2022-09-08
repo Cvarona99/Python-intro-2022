f33126527ac8ae9b004b03d87d711480dc0eb6b0
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-08 23:52:58 +0000

    Thu, Sep 8, 2022, 11:52 PM +00:00

--- loops.c
+++ loops.c
@@ -6,10 +6,10 @@ int main()
 
 int max = get_int("Max: \n");
 int i;
-    for (i= 0; i < max; i++)
+    for ( max;)
     {
         printf("%i\n", i);
     }
-    printf("You printed up to the number %i", i - 1);
+
     return 0;
 }
