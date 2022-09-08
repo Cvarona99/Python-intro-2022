cfe4e9c29755d858f7dd4a05f619d9099036e6aa
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-08 00:14:02 +0000

    Thu, Sep 8, 2022, 12:14 AM +00:00

--- week3/recursion.c
+++ week3/recursion.c
@@ -12,8 +12,18 @@ int main()
 
 void draw(int n)
 {
+    if (n <= 0)
+    {
+        return;
+    }
 
     draw(n - 1)
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 
 
 }
