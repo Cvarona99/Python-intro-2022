f078483c017af7565699e6728cf113052d29528d
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-08 23:16:10 +0000

    Thu, Sep 8, 2022, 11:16 PM +00:00

--- population.c
+++ population.c
@@ -4,14 +4,28 @@
 int main()
 {
     //prompting user for a starting # of llamas
+int n;
+do
+{
+     n = get_int("Number of llamas: \n");
+}
+while (n < 9);
 
-    int n = get_int("Number of llamas: \n");
 
 
-    //prompting for an ending # of llamas
 
-    int end = get_int("Ending population size: \n")
+  //prompting for an ending # of llamas
+  int end;
+do
+{
+     end = get_int("Ending population size: \n")
+}
+while (end < n);
 
 
-    
+
+
+
+
+
 }
