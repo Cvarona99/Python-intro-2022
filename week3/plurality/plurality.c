4f38ac2c3891bb45ab09cdf76e06c858c23ffe64
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-09 01:26:09 +0000

    Fri, Sep 9, 2022, 1:26 AM +00:00

--- week3/plurality/plurality.c
+++ week3/plurality/plurality.c
@@ -66,17 +66,14 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    
+
     for (int i = 0; i < candidate_count; i++)
     {
         if( strcmp(name, candidates[i].name))
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+       
     }
 
 
