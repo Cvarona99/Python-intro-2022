95aee433d059e699d5f323ffa8d70a1513759cbc
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-09 19:04:17 +0000

    Fri, Sep 9, 2022, 7:04 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -128,7 +128,7 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    // TODO 
 
     for (int i = 0; i < candidate_count; i++)
     {
