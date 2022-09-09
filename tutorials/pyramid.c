1708003901b6e34a77e8e5c0e5c32d22290e40cb
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-09 00:52:31 +0000

    Fri, Sep 9, 2022, 12:52 AM +00:00

--- tutorials/pyramid.c
+++ tutorials/pyramid.c
@@ -8,11 +8,19 @@ int main()
 
     int n = get_int("Number of rows: \n");
 
-    for (i = 0; i <= n -1; i++ )
+    for (i = 0; i <= n; i++ )
     {
         for (j = 0; j <= 2*n - 1; j++)
         {
-            printf("*");
+            if (j >= n-(i - 1) && j <= n + (i - 1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+
         }
         printf("\n");
     }
