55048fe04f7c7acfb90e89c82ffa4a54e8c4d8ed
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-09 20:04:52 +0000

    Fri, Sep 9, 2022, 8:04 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -132,7 +132,11 @@ bool vote(int voter, int rank, string name)
 
     for  (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp )
+        if (strcmp(candidate[i].name, name) == 0 )
+        {
+            
+            return true;
+        }
     }
 
     return false;
