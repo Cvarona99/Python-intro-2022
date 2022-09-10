113eb1706abc05e399b8b2702b395728fa9940d7
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-10 22:29:51 +0000

    Sat, Sep 10, 2022, 10:29 PM +00:00

--- pset1/cash/credit/credit.c
+++ pset1/cash/credit/credit.c
@@ -9,3 +9,13 @@ int main(void)
     long number = get_long("what's your credit card number: ")
     check sum(number);
 }
+
+
+bool check_sum(long num)
+{
+    int total = 0;
+    while()
+    {
+        total += num % 
+    }
+}
