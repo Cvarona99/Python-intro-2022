f7d10ed99fc547efe123f12db4e76b9c35d988a4
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-10 22:28:09 +0000

    Sat, Sep 10, 2022, 10:28 PM +00:00

--- pset1/cash/credit/credit.c
+++ pset1/cash/credit/credit.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// prototypes
+check_sum(long num);
+
 int main(void)
 {
     long number = get_long("what's your credit card number: ")
+    check sum(number);
 }
