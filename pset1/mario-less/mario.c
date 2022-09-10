3600569a40b9f6a75fc6a71370992c0b5f032ed9
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-10 05:39:48 +0000

    Sat, Sep 10, 2022, 5:39 AM +00:00

--- pset1/mario-less/mario.c
+++ pset1/mario-less/mario.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // Get height input
+    int height;
+
+    do
+    {
+        n = get_int("Height: ");
+
+    }
+    while (n < 1 || n > 8);
 }
