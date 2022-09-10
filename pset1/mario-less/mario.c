7c53331d78775aac5aeb893bb12499431aed68cc
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-10 06:15:52 +0000

    Sat, Sep 10, 2022, 6:15 AM +00:00

--- pset1/mario-less/mario.c
+++ pset1/mario-less/mario.c
@@ -15,9 +15,9 @@ int main(void)
 
     for ( int i = 0; i < height; i++)
     {
-        for ( int j = 0; j < height; j++)
+        for ( int j = 0; j < height - 1; j++)
         {
-            
+
 
 
         }
