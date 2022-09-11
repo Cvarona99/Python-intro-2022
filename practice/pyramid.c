fdd2db6e1256a1dfb19aa4e718ad911b686b7f22
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-11 05:06:47 +0000

    Sun, Sep 11, 2022, 5:06 AM +00:00

--- practice/pyramid.c
+++ practice/pyramid.c
@@ -8,9 +8,9 @@ int main()
 
     for (i = 0; i < height; i++)
     {
-        for (j = 0; j <= i; j++)
+        for (j = 1; j <= i; j++)
         {
-            printf("*");
+            printf("%i", j);
 
         }
     printf("\n");
