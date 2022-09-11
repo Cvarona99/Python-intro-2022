8f28c582bc8f408f40f5ed3ae524883f2e5f82ba
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-11 01:11:46 +0000

    Sun, Sep 11, 2022, 1:11 AM +00:00

--- practice/userNum.c
+++ practice/userNum.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int main()
+{
+    int n = get_int("How many natural number?: ");
+    int i;
+
+    for  (i = 1; i <= n; i++)
+    {
+        sum += i;
+        
+    }
+
+
+
+
+}
