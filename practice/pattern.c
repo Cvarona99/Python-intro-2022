6e464351e20860e187daa2f54790fc8a641fe891
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-11 04:57:04 +0000

    Sun, Sep 11, 2022, 4:57 AM +00:00

--- practice/pattern.c
+++ practice/pattern.c
@@ -3,8 +3,16 @@
 
 int main()
 {
-    int i, j = 0 
+    int i, j = 0;
+    int height = get_int("Enter a height ");
 
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j <= i; j++)
+        printf("%i\n", j);
+
+    }
+    printf("%i", i); 
 
 
 }
