53d6ea9232b5be180233376f798ee687610951e0
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-11 05:38:17 +0000

    Sun, Sep 11, 2022, 5:38 AM +00:00

---
+++ practice/box.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int i, j = 0;
+
+
+    for (i = 1; i <= 4; i++)
+    {
+        for (j = 1; j <= 4; j++)
+        {
+            printf("*  ");
+
+        }
+    printf("\n");
+    }
+
+
+
+}
