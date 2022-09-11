e3031c5a9678244e6dba54df80a3359496f6025d
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-11 01:31:01 +0000

    Sun, Sep 11, 2022, 1:31 AM +00:00

--- practice/keyboard.c
+++ practice/keyboard.c
@@ -4,13 +4,15 @@
 int main()
 {
     int i, n, sum = 0;
+
     float avg;
+
 
     printf("input 10 numbers: \n");
     for (int i = 0; i<10; i++)
     {
         printf("Number _ %i : ", i);
-        n = get_int( 
+        n = get_int("
     }
 
 
