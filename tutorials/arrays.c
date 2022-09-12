cce5da4fae9dc72e613b630a30024ed8954588a9
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-12 01:50:54 +0000

    Mon, Sep 12, 2022, 1:50 AM +00:00

--- tutorials/arrays.c
+++ tutorials/arrays.c
@@ -3,6 +3,6 @@
 
 int main()
 {
-    int myGrades[]; {10 , 12, 14, 15 ,16, 17, 54, 34, 23}
-    printf("%i ", myGrades[]);
+    int myGrades[] = {10 , 12, 14, 15 ,16, 17, 54, 34, 23};
+    printf("%i ", myGrades);
 }
