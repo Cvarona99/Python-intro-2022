c7f5ca38d8ed26748df6ee473cceb50deeadea1e
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-12 20:20:51 +0000

    Mon, Sep 12, 2022, 8:20 PM +00:00

--- week2/status.c
+++ week2/status.c
@@ -5,7 +5,13 @@ int main (int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Missing command line argument\n" )
+        printf("Missing command line argument\n" );
+        return 1;
+    }
+    else
+    {
+        printf("Hello , %s\n", argv[1]);
+        return 0; 
     }
 
 
