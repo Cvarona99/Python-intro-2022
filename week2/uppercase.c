39b7c7dc5be95b403f9a9740139946d6be388715
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-12 19:56:00 +0000

    Mon, Sep 12, 2022, 7:56 PM +00:00

--- week2/uppercase.c
+++ week2/uppercase.c
@@ -9,14 +9,7 @@ int main(void)
     printf("After: ");
     for (int i = 0; i < strlen(s); i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            printf("%c", s[i] - 32);
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
+    printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
