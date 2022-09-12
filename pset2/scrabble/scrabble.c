23d65cf82971cee55d7412899db640d362e0480d
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-12 22:47:58 +0000

    Mon, Sep 12, 2022, 10:47 PM +00:00

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -23,5 +23,12 @@ int main(void)
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    //Compute and return score for string
+    int sum = 0;
+    for (int i = 0; i < strlen(word; i++)
+    {
+        
+    }
+
+
 }
