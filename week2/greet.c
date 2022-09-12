f919d3dbcea83477882601c8a28081dbe60aac67
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-12 20:10:28 +0000

    Mon, Sep 12, 2022, 8:10 PM +00:00

--- week2/greet.c
+++ week2/greet.c
@@ -4,6 +4,13 @@
 int main(int argc, string argv[])
 {
 
-    string name = get_string("what's your name: ");
-    printf("Hello, %s\n", argv[1]);
+
+    if (argc == 2)
+    {
+        printf("Hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("Hello World\n");
+    }
 }
