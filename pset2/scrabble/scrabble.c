82599d1e0bd0b133f902f942b174bbda016d36c7
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-12 01:42:27 +0000

    Mon, Sep 12, 2022, 1:42 AM +00:00

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -18,7 +18,7 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // TODO: Print the winner      uwu
 }
 
 int compute_score(string word)
