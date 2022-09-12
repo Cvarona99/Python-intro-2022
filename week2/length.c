eacb5ab9a1c715ba4351d44975ab2a38ad307d3f
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-12 19:50:09 +0000

    Mon, Sep 12, 2022, 7:50 PM +00:00

--- week2/length.c
+++ week2/length.c
@@ -2,14 +2,10 @@
 #include <cs50.h>
 #include <string.h>
 
+
 int main(void)
 {
     string name = get_string("What's your name? ");
-
-    int n = 0;
-    while (name[n] != '\0')
-    {
-        n++;
-    }
-    printf("%i\n", n);
+    int n = strlen(name);
+    printf("string length: %i\n" , n);
 }
--- week2/uppercase.c
+++ week2/uppercase.c
@@ -7,11 +7,11 @@ int main(void)
 {
     string s = get_string("Before: ");
     printf("After: ");
-    for( int i = 0, n = strlen(s); i < n; i++)
+    for (int i = 0; i < strlen(s); i++)
     {
-        printf("%c", toupper(s[i]));
-
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            printf("%c", s[i])
+        }
     }
-    printf("\n");
-
 }
