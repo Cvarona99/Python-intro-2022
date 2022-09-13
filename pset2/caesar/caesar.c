74574fc57f19709b0679b21326b4377734d292c5
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-13 22:48:43 +0000

    Tue, Sep 13, 2022, 10:48 PM +00:00

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -10,9 +10,23 @@ char rotate(char p, int k);
 int main(int argc, string argv[])
 {
     // Get the key
-    if (ar)
+    if (argv != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
-
-
-
+bool only_digits(string s)
+{
+    int length = strlen(s);
+    for (int i = 0; i < length; i++)
+    {
+        if (!isdigit(s[i]))
+        
+    }
 }
