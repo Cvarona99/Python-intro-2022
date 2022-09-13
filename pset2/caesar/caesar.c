a4b3f9180519fc7a8e95f9c7319afd18c992ede1
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-13 22:39:28 +0000

    Tue, Sep 13, 2022, 10:39 PM +00:00

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h> 
+#include <string.h>
+
+bool only_digits(string s);
+char rotate(char p, int k);
 
 int main(int argc, string argv[])
 {
     // Get the key
-
+    if (ar)
 
 
 
