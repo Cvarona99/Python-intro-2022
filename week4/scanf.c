b31eb23461247e9d82794b4cd458cafc828eb17f
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-14 17:02:09 +0000

    Wed, Sep 14, 2022, 5:02 PM +00:00

--- week4/scanf.c
+++ week4/scanf.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 
-    int x;
-    printf("x: ");
-    scanf("%i", &x;)
-    printf("x: %i\n", x); 
+    char *s;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s); 
 }
