534a965da72198c7560963bc26cf845ed595e4e8
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-14 00:05:11 +0000

    Wed, Sep 14, 2022, 12:05 AM +00:00

--- practice/squarefunct.c
+++ practice/squarefunct.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <cs50.h>
 
-double square( double number)
+double square( double number);
 int main()
 {
     int num = get_int("input any number and it will be squared.");
     double squared = square(num);
-    printf("The square root of %i is %i\n", num, squared);
+    printf("The square root of %i is %f\n", num, squared);
 }
 
 double square( double number)
