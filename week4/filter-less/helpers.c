db616f258fe9a95c0b6fa2731e5bc3fa7b6365c5
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-15 20:46:53 +0000

    Thu, Sep 15, 2022, 8:46 PM +00:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -39,7 +39,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
             int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
             int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
-            // set minumum value of 
+            // set minumum value to 255 
              image[i][j].rgbtRed = fmin(255, sepiaRed);
              image[i][j].rgbtGreen = fmin(255, sepiaGreen);
              image[i][j].rgbtBlue = fmin(255, sepiaBlue);
