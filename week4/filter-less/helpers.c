a9cd8678609be21aaf931f8c4516cc1c7b91ff7b
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-15 23:45:08 +0000

    Thu, Sep 15, 2022, 11:45 PM +00:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -123,7 +123,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-   
+     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+        }
+    }
 
     return;
 }
