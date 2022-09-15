5947204c5d78b950b97bb0f9d821cd8d34796cfc
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-15 20:30:43 +0000

    Thu, Sep 15, 2022, 8:30 PM +00:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -37,8 +37,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float originalBlue  = image[i][j].rgbtBlue;
 
             int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
-            int sepiaGreen = (.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
-            int sepiaBlue = (.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
 
             if (sepiaRed > 255 || sepiaGreen > 255 || sepiaBlue > 255)
             {
