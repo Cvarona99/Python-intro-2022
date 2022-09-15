0631acaa26e4762d9d0cff85909eedd8102fc542
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-15 01:04:59 +0000

    Thu, Sep 15, 2022, 1:04 AM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
 
     uint8_t header[HEADER_SIZE];
-    fread()
+    fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 
 
 
