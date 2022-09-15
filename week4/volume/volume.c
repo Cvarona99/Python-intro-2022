a18912b18a4980741f03dac2c36716bc0a19fb5c
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-15 00:52:44 +0000

    Thu, Sep 15, 2022, 12:52 AM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -34,11 +34,14 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    
+    int headerbuffer[44];
+    fread()
+
+
 
     // TODO: Read samples from input file and write updated data to output file
-    fread[HEADER_SIZE];
-    fwrite[HEADER_SIZE];
+
+
 
     // Close files
     fclose(input);
