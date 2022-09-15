21c7af15c889f078b72627615f39caed6e84bba8
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-15 21:19:13 +0000

    Thu, Sep 15, 2022, 9:19 PM +00:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -5,7 +5,7 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     int average;
-    for (int i= 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
@@ -14,7 +14,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             float green = image[i][j].rgbtGreen;
             float blue  = image[i][j].rgbtBlue;
 
-            average  = round(red + green + blue) / 3.0;
+            average  = round((red + green + blue) / 3.0);
             image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
 
         }
