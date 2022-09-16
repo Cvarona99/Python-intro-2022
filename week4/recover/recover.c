630338125834a37a66e0d8b5d711fceae4f1f358
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-16 23:30:46 +0000

    Fri, Sep 16, 2022, 11:30 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -23,7 +23,10 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, sizeof(BYTE), 512, f) == 512)
     {
-        if ()
+        if (buffer[0] == 0xff && buffer [1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
+        {
+            
+        }
     }
 
 }
