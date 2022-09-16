38b56c7ea7aeb7b1ebf966c0c2f6d2d0ccb38711
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-16 23:55:09 +0000

    Fri, Sep 16, 2022, 11:55 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -49,6 +49,7 @@ int main(int argc, char *argv[])
             }
             else
             {
+                fwrite(buffer, sizeOf(BYTE), bytesRead, img);
                 
             }
         }
