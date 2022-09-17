f707213c7b871059e6ea39452ad04b1c4a66bc8a
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 02:30:41 +0000

    Sat, Sep 17, 2022, 2:30 AM +00:00

--- tutorials/clock.c
+++ tutorials/clock.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
+int main()
+{
+    int h, m, s;
+    int d = 1000; // delay of 1000 milliseconds
+    printf("Set Time: "\n);
+    scanf("%i)
 
+
+
+}
