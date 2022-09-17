559ded8cb4989d4a7bc2146806aa6bfc54f7ddf3
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 00:55:18 +0000

    Sat, Sep 17, 2022, 12:55 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -20,7 +20,8 @@ int main(int argc, char *argv[])
 
     typedef uint8_t BYTE;
     BYTE buffer[512];
-    int counter, bytesRead = 0;
+    int counter = 0;
+    bytesRead = 0;
     char filename[8];
     FILE *img = NULL;
 
