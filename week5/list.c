9807df0cd16b86b2035ffab6aa85921b3e30c181
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 17:53:37 +0000

    Sat, Sep 17, 2022, 5:53 PM +00:00

--- week5/list.c
+++ week5/list.c
@@ -3,28 +3,35 @@
 
 int main()
 {
+    // Dynamically allocate array of size 3
   int *list = malloc(3 * sizeof(int));
   if (list == NULL)
     return 1;
 
-
+// Assign 3 numbers to that array
 list [0] = 1;
 list [1] = 2;
 list [2] = 3;
 
 // Time passes and you want to add to array dynamically
 
+
+// Allocate new array of size 4
 int *tmp = malloc(4 * sizeof(int));
 if (tmp == NULL)
 return 2;
 
+
+// Copy numbers from old array int new array
 for (int i = 0; i < 3; i++)
 {
     tmp[i] = list[i];
 }
 
+// Add fourth number to new array
 tmp[3] = 4;
 
+// Free old array
 free(list);
 
 list = tmp;
