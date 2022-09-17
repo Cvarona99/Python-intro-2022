e08b37a373a30cde0bc8ae3bb87abbca647a3369
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 02:39:24 +0000

    Sat, Sep 17, 2022, 2:39 AM +00:00

--- tutorials/clock.c
+++ tutorials/clock.c
@@ -6,12 +6,35 @@ int main()
     int h, m, s;
     int d = 1000; // delay of 1000 milliseconds
     printf("Set Time: "\n);
-    scanf("%i%i%i, &h, &m, &s);
-    if (h > 12 || m > 60 || s > 60)\
+    scanf("%i%i%i", &h, &m, &s);
+    if (h > 12 || m > 60 || s > 60)
     {
         printf("Invalid time\n");
-        return 1; 
+        return 1;
     }
+    while(1) // infinite loop
+    {
+        s++
+        if(s > 59)
+        {
+            m++;
+            s = 0;
+        }
+        if (m > 59)
+        {
+            h++;
+            m = 0;
+        }
+        if (h > 12)
+        {
+            h = 1;
+        }
+
+        printf("Clock: ")
+        printf("%02i:%2i:%2i", h, m, s);
+
+    }
+
 
 
 
