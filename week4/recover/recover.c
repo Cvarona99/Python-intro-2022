cc00e54f2be972febd833336d737a11765560d28
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 00:32:25 +0000

    Sat, Sep 17, 2022, 12:32 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -30,10 +30,10 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer [1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
             // If first
-            if(counter == 0;)
+            if(counter == 0)
             {
                 // Create new JPEG
-                sprintf(filename, "%03i.jpg", count);
+                sprintf(filename, "%03i.jpg", counter);
                 img = fopen(filename, "w");
                 fwrite(buffer, sizeof(BYTE), bytesRead, img);
                 counter++;
