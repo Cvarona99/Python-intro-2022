75fa89e25fbfa4296bc5577e78f4776479f14113
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 01:26:25 +0000

    Sat, Sep 17, 2022, 1:26 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     FILE *img = NULL;
 
 
-    while (fread(buffer, /*1*/sizeof(BYTE), 512, f) == 512)
+    while (fread(buffer, 1, 512, f) == 512)
     {
         if (buffer[0] == 0xff && buffer [1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
