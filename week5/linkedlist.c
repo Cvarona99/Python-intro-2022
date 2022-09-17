12fd00330066a13d009d7f1a75d3a7738a75c202
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 18:38:14 +0000

    Sat, Sep 17, 2022, 6:38 PM +00:00

--- week5/linkedlist.c
+++ week5/linkedlist.c
@@ -53,4 +53,12 @@ int main(void)
     {
         printf("%i\n", tmp->number);
     }
+
+    // Free linked list
+    while (list != NULL)
+    {
+        node* tmp = list->next;
+        free(list);
+        list = tmp; 
+    }
 }
