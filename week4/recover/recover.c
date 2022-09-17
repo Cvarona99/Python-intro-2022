0076abb26b8c1d9e71dfbe40346a3e9fb2e4abb9
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 01:58:26 +0000

    Sat, Sep 17, 2022, 1:58 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-
+// Open memory card file
     FILE *f = fopen(argv[1], "r");
     if (!f)
     {
@@ -24,7 +24,8 @@ int main(int argc, char *argv[])
     char filename[8];
     FILE *img = NULL;
 
-
+// Repeat until end of the card
+// read 512 bytes into a buffer
     while (fread(buffer, sizeof(BYTE), 512, f))
     {
         if (buffer[0] == 0xff && buffer [1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
@@ -33,12 +34,13 @@ int main(int argc, char *argv[])
             img = fopen(filename, "w");
             counter++;
         }
+        // New jPEG / already open 
         if (img != NULL)
         {
             fwrite(buffer, sizeof(BYTE), 512, img);
         }
     }
-
+// close all files at the end
     fclose(f);
     fclose(img);
     return 0;
