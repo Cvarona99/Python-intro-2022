d3cf569b20aed878d7e6d9d5870c750d938a1204
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 02:33:16 +0000

    Sat, Sep 17, 2022, 2:33 AM +00:00

--- tutorials/clock.c
+++ tutorials/clock.c
@@ -6,7 +6,12 @@ int main()
     int h, m, s;
     int d = 1000; // delay of 1000 milliseconds
     printf("Set Time: "\n);
-    scanf("%i)
+    scanf("%i%i%i, &h, &m, &s);
+    if (h > 12 || m > 60 || s > 60)\
+    {
+        printf("Invalid time\n");
+        return 1; 
+    }
 
 
 
