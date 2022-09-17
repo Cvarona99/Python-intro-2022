a84b02071ef830b7777849a73b6845739d545aed
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 23:55:52 +0000

    Sat, Sep 17, 2022, 11:55 PM +00:00

--- tutorials/linkedlist.c
+++ tutorials/linkedlist.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct sPerson
+{
+    int age;
+}
+sPerson;
+
 int main(void)
 {
 
     // Dynamic allocation of memory - allocation of memory only as needed
-    
+
 
 }
