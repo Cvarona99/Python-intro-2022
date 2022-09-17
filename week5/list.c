5dec9ddf2003461a1ba5777067755302b5436bc4
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-17 17:40:43 +0000

    Sat, Sep 17, 2022, 5:40 PM +00:00

--- week5/list.c
+++ week5/list.c
@@ -4,4 +4,18 @@
 int main()
 {
   int *list = malloc(3 * sizeof(int));
+  if (list == NULL)
+    return 1;
+
+
+list [0] = 1;
+list [1] = 2;
+list [2] = 3;
+
+// Time passes and you want to add to array dynamically
+
+int * = malloc(4 * sizeof(int));
+list[3] = 4;
 }
+
+
