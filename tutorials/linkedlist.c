bf38a9f037ee0c1c4483ca2edd39687f0121772c
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-18 00:07:53 +0000

    Sun, Sep 18, 2022, 12:07 AM +00:00

--- tutorials/linkedlist.c
+++ tutorials/linkedlist.c
@@ -7,10 +7,29 @@ struct sPerson
 }
 sPerson;
 
+struct sPerson* getNewPerson(const int age)
+{
+
+    struct sPerson* newPerson = NULL;
+    newPerson = malloc(sizeof(struct sPerson));
+    newPerson->age = age;
+    printf("Created new person at %p\n", newPerson);
+    return newPerson;
+}
+
+void printPerson(const struct sPerson* person, const char* comment)
+{
+    if (person == NULL)
+    {
+        return 1; 
+    }
+
+}
+
 int main(void)
 {
 
     // Dynamic allocation of memory - allocation of memory only as needed
-    
+
 
 }
