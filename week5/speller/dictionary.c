f090d891f94561ba0a53787a24dc459e320bc324
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-19 18:19:47 +0000

    Mon, Sep 19, 2022, 6:19 PM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -62,7 +62,7 @@ bool load(const char *dictionary)
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
-            return false;
+            
         }
         // Copy word into node
         strcopy(n->word, word);
