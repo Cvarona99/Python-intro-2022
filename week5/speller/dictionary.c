ea7c2ef2821fcb86c5b7150fd642ac63ff8a7878
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-19 19:05:34 +0000

    Mon, Sep 19, 2022, 7:05 PM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -74,7 +74,7 @@ bool load(const char *dictionary)
         // Copy word into node
         strcpy(n->word, word);
         hash_value = hash(word);
-
+        
         if(table[hash_value] == NULL)
         {
             n->next = NULL;
