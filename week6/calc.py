bb9efcc7010716673f684bf5ca2813f8aa0c9a8e
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-19 22:58:28 +0000

    Mon, Sep 19, 2022, 10:58 PM +00:00

--- week6/calc.py
+++ week6/calc.py
@@ -1,10 +1,8 @@
-try:
-x = int(input("x: "))
-except ValueError:
-    print("That's not an int!")
-    exit()
-try:
-y = int(input("y: "))\
-except:
-print(x + y)
+from cs50 import get_int
+
+x = get_int("x: ")
+y = get_int("y: ")
+
+z = x / y
+print(z)
 
