84a7b2888106532f422b5d7a7c813878975342a1
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-20 17:56:09 +0000

    Tue, Sep 20, 2022, 5:56 PM +00:00

--- week6/mario.py
+++ week6/mario.py
@@ -1,13 +1,2 @@
-from cs50 import get_int
-
-
-def main():
-    for i in range(n):
-        print("#")
-
-def get_height():
-    while True:
-        n = get_int("Height: ")
-        if n > 0:
-            break
-        return n
+for i in range(4):
+    print("?", " ")
