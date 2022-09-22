3e63687dd7e080f543999c981deff3d279c2c0ea
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-22 20:43:15 +0000

    Thu, Sep 22, 2022, 8:43 PM +00:00

--- pythontut/operators.py
+++ pythontut/operators.py
@@ -1,11 +1,2 @@
-weight = int(input("Weight: "))
-unit = input("(K)g or (L)b: ")
-
-if unit.lower == "L":
-    converted = weight * 0.45
-    print ("Weight in Kg: " + str(converted))
-elif unit.lower == "K":
-    converted = weight / 0.45
-    print("Weight in Lbs: " + str(converted))
-else:
-    print("error, invalid input")
+result = 1 + 2
+print("1 + 2 is " + str(result))
