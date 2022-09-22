ce2f66fbfd22057c555a77f387cbfd5139714087
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-22 22:00:37 +0000

    Thu, Sep 22, 2022, 10:00 PM +00:00

--- breakCont.py
+++ breakCont.py
@@ -2,7 +2,7 @@ number = 0
 
 while number < 10:
     if number == 5:
-        break   # breaks the loop >:3
+        break   #! breaks the loop >:3 Next function is executed immediately
     number += 1
     print(number)
 
@@ -10,4 +10,4 @@ while number < 10:
 #   if number == 5:
 #       continue #! fulfills the loop until the if statement if met, then executes the next function
 #   number += 1
-#
+#   print(number)
