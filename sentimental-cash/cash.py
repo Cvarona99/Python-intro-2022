e542242a5c9146e8eef71d810dd989a5f9875e9d
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-23 19:35:36 +0000

    Fri, Sep 23, 2022, 7:35 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,31 +2,44 @@ from cs50 import get_float
 
 def main():
     # Ask how many dollars the customer is owed
-    int dollars = get_dollars()
+    dollars = get_dollars()
 
     # Calculate the number of quarters
-    int quarters = calculate_quarters(dollars)
+    quarters = calculate_quarters(dollars)
     dollars = dollars - (quarters * 25)
 
     # Calculate the number of dimes
-    int dimes = calculate_dimes(dollars)
+    dimes = calculate_dimes(dollars)
     dollars = dollars - (dimes * 10)
 
     # Calculate the number of nickels
-    int nickels = calculate_nickels(dollars)
+    nickels = calculate_nickels(dollars)
     dollars = dollars - (nickels * 5)
 
     # Calculate the number of pennies
-    int pennies = calculate_pennies(dollars)
+    pennies = calculate_pennies(dollars)
     dollars = dollars - (pennies * 1)
 
     # Sum coins
-    int coins = quarters + dimes + nickels + pennies
+    coins = quarters + dimes + nickels + pennies
 
-def get_cents():
+def get_dollars():
     while True:
         dollars = get_float("Change?: ")
         if dollars > 0.00:
             break
         else:
             continue
+    return dollars
+
+def calculate_quarters(dollars):
+    while dollars >= 0.25:
+        quarters + 1
+        dollars -= 0.25
+    return quarters
+
+def calculate_dimes(dollars):
+    while dollars >= 0.10:
+        dimes + 1
+        dollars -= 0.10
+    return dimes
