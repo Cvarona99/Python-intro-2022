81d6b95b85c65fa52007d979a55e87a2e3ddca0a
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-23 01:30:51 +0000

    Fri, Sep 23, 2022, 1:30 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,11 +1,8 @@
 def main():
     size  = get_height()
-    for i in range(size):
-        for size in range(>size):
-            print(".")
-        for j in range(i+1):
-            print("#", end="")
-        print()
+    for i in range(0, size):
+         
+
 
 def get_height():
     while True:
