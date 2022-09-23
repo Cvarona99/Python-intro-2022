a7a17df184cf5e64fdedbed632306faff1f77177
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-23 00:52:48 +0000

    Fri, Sep 23, 2022, 12:52 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,9 +1,14 @@
-while True:
-    height = int(input("Height: "))
-    if height < 1 or height > 8:
-        continue
-    else:
-        break
-for i in range(height):
-    for j in range(i):
-        print("#")
+def main():
+    for i in range(height):
+        for j in range(i):
+            print("#")
+        print(" ")
+
+def get_height():
+    while True:
+        height = int(input("Height: "))
+        if height < 1 or height > 8:
+            continue
+        else:
+            break
+        return height
