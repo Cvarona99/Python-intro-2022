034c31dd91467062554adf81d3529807e9867148
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-23 21:17:28 +0000

    Fri, Sep 23, 2022, 9:17 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,3 +3,10 @@ from cs50 import get_string
 def main():
     text = get_string("Text: ")
     letters = count_letters(text)
+    sentences = count_sentences(text)
+    words = count_words(texts)
+
+    L = letters / words * 100
+    S = sentences / words * 100
+
+    index = ro
