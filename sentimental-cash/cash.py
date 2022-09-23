0c6d05888627c54dc59907f162f76f97b613df6d
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-23 19:45:52 +0000

    Fri, Sep 23, 2022, 7:45 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -6,19 +6,19 @@ def main():
 
     # Calculate the number of quarters
     quarters = calculate_quarters(dollars)
-    dollars = dollars - (quarters * 25)
+    dollars = dollars - (quarters * 0.25)
 
     # Calculate the number of dimes
     dimes = calculate_dimes(dollars)
-    dollars = dollars - (dimes * 10)
+    dollars = dollars - (dimes * 0.10)
 
     # Calculate the number of nickels
     nickels = calculate_nickels(dollars)
-    dollars = dollars - (nickels * 5)
+    dollars = dollars - (nickels * 0.05)
 
     # Calculate the number of pennies
     pennies = calculate_pennies(dollars)
-    dollars = dollars - (pennies * 1)
+    dollars = dollars - (pennies * 0.01)
 
     # Sum coins
     coins = quarters + dimes + nickels + pennies
