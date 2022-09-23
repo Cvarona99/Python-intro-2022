19596eac5956f47caf2e535521c073b5c3f26f5b
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-23 19:55:43 +0000

    Fri, Sep 23, 2022, 7:55 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,63 +1,63 @@
 from cs50 import get_float
 
 def main():
-    # Ask how many dollars the customer is owed
-    dollars = get_dollars()
+    # Ask how many cents the customer is owed
+    cents = get_cents()
 
     # Calculate the number of quarters
-    quarters = calculate_quarters(dollars)
-    dollars = dollars - (quarters * 0.25)
+    quarters = calculate_quarters(cents)
+    cents = cents - (quarters * 25)
 
     # Calculate the number of dimes
-    dimes = calculate_dimes(dollars)
-    dollars = dollars - (dimes * 0.10)
+    dimes = calculate_dimes(cents)
+    cents = cents - (dimes * 10)
 
     # Calculate the number of nickels
-    nickels = calculate_nickels(dollars)
-    dollars = dollars - (nickels * 0.05)
+    nickels = calculate_nickels(cents)
+    cents = cents - (nickels * 5)
 
     # Calculate the number of pennies
-    pennies = calculate_pennies(dollars)
-    dollars = dollars - (pennies * 0.01)
+    pennies = calculate_pennies(cents)
+    cents = cents - (pennies * 1)
 
     # Sum coins
     coins = quarters + dimes + nickels + pennies
     print(coins)
 
-def get_dollars():
+def get_cents():
     while True:
-        dollars = get_float("Change?: ")
-        if dollars > 0.00:
+        cents = get_float("Change?: ")
+        if cents > 0:
             break
         else:
             continue
-    return dollars
+    return cents
 
-def calculate_quarters(dollars):
+def calculate_quarters(cents):
     quarters = 0
-    while dollars >= 0.25:
-        quarters + 1
-        dollars -= 0.25
+    while cents >= 25:
+        quarters += 1
+        cents -= 25
     return quarters
 
-def calculate_dimes(dollars):
+def calculate_dimes(cents):
     dimes = 0
-    while dollars >= 0.10:
-        dimes + 1
-        dollars -= 0.10
+    while cents >= 10:
+        dimes += 1
+        cents -= 10
     return dimes
 
-def calculate_nickels(dollars):
+def calculate_nickels(cents):
     nickels = 0
-    while dollars >= 0.05:
-        nickels + 1
-        dollars -= 0.05
+    while cents >= 5:
+        nickels += 1
+        cents -= 5
     return nickels
 
-def calculate_pennies(dollars):
+def calculate_pennies(cents):
     pennies = 0
-    while dollars >= 0.01:
-        pennies + 1
-        dollars -= 0.01
+    while cents >= 1:
+        pennies += 1
+        cents -= 1
     return pennies
 main()
