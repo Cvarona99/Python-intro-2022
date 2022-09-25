3f0c3f4e0cdea4362905c669fc6f6f41e8f8300e
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-25 18:04:16 +0000

    Sun, Sep 25, 2022, 6:04 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if sys.argv < 3:
+    if len(sys.argv) < 3:
         print("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
