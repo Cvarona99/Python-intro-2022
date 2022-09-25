fca706d0e56fbdc9f31077e30bafef76b72f99f1
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-25 21:55:01 +0000

    Sun, Sep 25, 2022, 9:55 PM +00:00

--- praccy/randomguess.py
+++ praccy/randomguess.py
@@ -10,7 +10,21 @@ def guess(x):
         elif guess > randomNum:
             print("Too high")
 
-    print (" You have guessed correctly!")
+    print ("You have guessed correctly!")
+
+
+def computerGuess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input(f"Is {guess} too high, too low, or correct??").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print("The computer guessed correctly!")
 
 
 guess(10)
