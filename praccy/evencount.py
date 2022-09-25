d0fd3571e61b5125b88d9b10440da2ab8b558962
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-25 19:16:29 +0000

    Sun, Sep 25, 2022, 7:16 PM +00:00

--- praccy/evencount.py
+++ praccy/evencount.py
@@ -1,7 +1,6 @@
 counter = 0
-for x in range(10):
-    if x > 0:
-        if x %2 == 0:
-            counter += 1
+for x in range(1,10):
+    if x %2 == 0:
+        counter += 1
             print(x)
 print("We have ", counter, "even numbers!")
