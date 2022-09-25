fc0df3b407e75ed2c1c00b80190802319f48d750
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-09-25 21:28:30 +0000

    Sun, Sep 25, 2022, 9:28 PM +00:00

--- praccy/madlib.py
+++ praccy/madlib.py
@@ -1,7 +1,9 @@
 adj1 = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
-famous_person("Famous person: ")
+famous_person = ("Famous person: ")
 
 madlib = f"Computer programming is so {adj1}! It makes me so excited all the time because\
-    I love to {Verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
