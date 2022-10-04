1d7775f95c9e5563a5b95d8214ad6a67f95f45c6
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-10-04 22:47:31 +0000

    Tue, Oct 4, 2022, 10:47 PM +00:00

--- pythontut/dateAndTime.py
+++ pythontut/dateAndTime.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from datetime import date
+import time
 
 now = datetime.now() # CURRENT DATE AND TIME
 today = date.today()
+clock = time.localtime()
 time = now.strftime("%H:%M:%S")
 
-print ("Current date: ", today , "\nTime: ", time)
+print ("Current date: ", today , "\nTime: ", clock )
