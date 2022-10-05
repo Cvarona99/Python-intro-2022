de54e8b4aa259d26cf346434d11981fead6d48f5
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-10-05 22:13:41 +0000

    Wed, Oct 5, 2022, 10:13 PM +00:00

--- pythontut/dateAndTime.py
+++ pythontut/dateAndTime.py
@@ -1,11 +1,22 @@
-from datetime import date
-import time
-import pytz 
+#! Program that displays the date as month day year and the time as eastern standard time
+
+from datetime import date, datetime, timedelta
+import pytz
 
 
-today = date.today()
-clock = time.localtime()
-time = time.strftime("%I:%M", clock)
 
 
-print ("Current date: ", today , "\nTime: ", time )
+NYC = pytz.timezone('US/Eastern') # Sets the timezone to EDT
+datetime_est = datetime.now(NYC)
+currentTime = datetime_est.strftime("%I:%M %p %Z") # Displays the time in 12 hour format
+today = date.today() - timedelta(days= 1) # Establishes the variable to day which is the date - 1 day because otherwise it displays one day ahead for some reason
+month_day_year = today.strftime("%m-%d-%Y") # Changes the format to month day year
+
+
+
+
+
+print("Current date: ", month_day_year , "\nTime: ", currentTime ) # Prints the current date and time
+
+
+
