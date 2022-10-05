f30d32fe7d29a48a25b70bbdb4ad1aebbbb8f2a3
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-10-05 22:19:25 +0000

    Wed, Oct 5, 2022, 10:19 PM +00:00

--- pythontut/dateAndTime.py
+++ pythontut/dateAndTime.py
@@ -6,17 +6,17 @@ import pytz
 
 
 
-
-NYC = pytz.timezone('US/Eastern') # Sets the timezone to EDT
-datetime_est = datetime.now(NYC)
-currentTime = datetime_est.strftime("%I:%M %p %Z") # Displays the time in 12 hour format
-today = date.today() - timedelta(days= 0) # Establishes the variable to day which is the date - 1 day because otherwise it displays one day ahead for some reason
-month_day_year = today.strftime("%m-%d-%Y") # Changes the format to month day year
-
-
-
-
 while True:
+    NYC = pytz.timezone('US/Eastern') # Sets the timezone to EDT
+    datetime_est = datetime.now(NYC)
+    currentTime = datetime_est.strftime("%I:%M:%S %p %Z") # Displays the time in 12 hour format
+    today = date.today() - timedelta(days= 0) # Establishes the variable to day which is the date - 1 day because otherwise it displays one day ahead for some reason
+    month_day_year = today.strftime("%m-%d-%Y") # Changes the format to month day year
+
+
+
+
+
     print("Current date: ", month_day_year , "\nTime: ", currentTime ) # Prints the current date and time
     time.sleep(1)
 
