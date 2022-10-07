e2cdf0e59f3d49c5329e76d4442cf65e8cdf698f
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-10-07 21:40:38 +0000

    Fri, Oct 7, 2022, 9:40 PM +00:00

--- pythontut/quizGame.py
+++ pythontut/quizGame.py
@@ -6,16 +6,19 @@ def main():
 
     while True:
         playing = input("Do you want to play? ")
-        if playing != "yes" or "no":
+        if playing != "yes" or if playing != "no" :
+
+
             print("type yes or no")
-            continue
+
+
         elif playing == "no":
             break
             return 1
         else:
             print("I took the wooooooock to Poland")
             break
-
+        break
 
 
 
