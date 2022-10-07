eb051a10fda7845a71b80b9f4925d28a50a68fab
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-10-07 21:27:13 +0000

    Fri, Oct 7, 2022, 9:27 PM +00:00

--- pythontut/quizGame.py
+++ pythontut/quizGame.py
@@ -3,6 +3,12 @@ print("Welcome to computer quiz!")
 
 while True:
     playing = input("Do you want to play? ")
+    if playing != "yes" or != "no":
+        print("type yes or no")
+        continue
+    else if playing = "no":
+        return 1
+
 
 
 
