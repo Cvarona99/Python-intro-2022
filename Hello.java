ae9332a76b35161d032ce25b7e22034ebd0d4ed3
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-11-03 22:14:14 +0000

    Thu, Nov 3, 2022, 10:14 PM +00:00

--- Hello.java
+++ Hello.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 class First {
-public static void main(String[] args) throws Exception {
-    try {
+public static void main(String[] args){
+
 
 
     Scanner k = new Scanner(System.in); // create scanner object
     System.out.println("Enter name");
     String name = k.nextLine(); // read user input
     System.out.println("Hello ," +name);
-
-}
-catch 
+    k.close();
 }
 }
