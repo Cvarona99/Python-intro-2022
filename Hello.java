d2d72f80c73e815457da5e8e264e5e0b0b4fcfdc
Cvarona99 <81775614+Cvarona99@users.noreply.github.com>
2022-11-03 22:05:19 +0000

    Thu, Nov 3, 2022, 10:05 PM +00:00

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 class First {
 public static void main(String[] args) throws Exception {
-    Scanner name = new Scanner(System.in); // create scanner object
+    Scanner k = new Scanner(System.in); // create scanner object
     System.out.println("Enter name");
-    String name = name.nextLine(); // read user input
+    String name = k.nextLine(); // read user input
     System.out.println("Hello ," +name);
-    }
+ }
 }
